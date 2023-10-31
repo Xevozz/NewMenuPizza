f665688532a880c5c42a0feddaac89338f8599a4
Abdirahman Abbas Sheikh Husein <abs001@edu.zealand.dk>
2023-10-31 14:34:35 +0100

    Jeg har lavet en Pizza og er igang med lave en Menu til Pizzaen

--- NewMenuPizza/Pizza/Pizza.cs
+++ NewMenuPizza/Pizza/Pizza.cs
@@ -2,5 +2,53 @@ namespace NewMenuPizza.Pizza;
 
 public class Pizza
 {
-    
-}
+    // instans felter
+
+    private string _navn;
+    private int _pris;
+    private int _pizzanummer;
+
+    // properties
+
+    private string Navn
+    {
+        get { return _navn; }
+        set { _navn = value; }
+    }
+
+    public int Pris
+    {
+        get { return _pris; }
+        set { _pris = value; }
+    }
+
+    private int Pizzanummer
+    {
+        get { return _pizzanummer; }
+        set { _pizzanummer = value; }
+    }
+
+    //  Default Constructor
+
+    public Pizza()
+    {
+        _navn = "";
+        _pris = 0;
+        _pizzanummer = 0;
+    }
+
+    public Pizza(string navn, int pris, int pizzanummer)
+    {
+        _navn = navn;
+        _pris = pris;
+        _pris = pizzanummer;
+
+    }
+
+    // toString 
+    public override string ToString()
+    {
+        return $"{{{nameof(Pris)}={Pris.ToString()}}}";
+    }
+
+ }
---
+++ NewMenuPizza/Pizza/PizzaRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Specialized;
+
+namespace NewMenuPizza.Pizza
+{
+    public class PizzaRepository
+    {
+        // instans felter
+
+        private Dictionary<int, Pizza> _pizzarepository;
+
+
+        Dictionary<int, Pizza> PizzaRepository
+        {
+          get { return _pizzarepository; }
+          set { _pizzarepository = value; }
+        }
+    }
+}
