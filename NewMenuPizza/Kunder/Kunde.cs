5f4c5ddad42a86e06fcf33bd78e0d5a31e052afe
Xevozz <Niklas.loev@gmail.com>
2023-10-31 14:23:22 +0100

    Kunde.cs

--- NewMenuPizza/Kunder/Kunde.cs
+++ NewMenuPizza/Kunder/Kunde.cs
@@ -1,6 +1,55 @@
 namespace NewMenuPizza.Kunder;
 
+//instans
+//properties (navn, adresse, tlf) 
+//kontruktør (liste) (public Kunde (int nr, string navn, string tlf)
+//ToString - til dictionary
+
 public class Kunde
 {
+    //instantfelt
+    private string _navn;
+    private string _adresse;
+    private string _tlf;
     
+    //properties
+
+    public string _Navn
+    {
+        get { return _navn; }
+        set { _navn = value;  }
+    }
+    
+    public string _Adresse
+    {
+        get { return _adresse; }
+        set { _adresse = value;  }
+    }
+    
+    public string _Tlf
+    {
+        get { return _tlf; }
+        set { _tlf = value;  }
+    }
+    
+    //konstruktør
+
+    public Kunde()
+    {
+        _navn = "";
+        _adresse = "";
+        _tlf = "";
+    }
+
+    public Kunde(string navn, string adresse, string tlf)
+    {
+        _navn = navn;
+        _adresse = adresse;
+        _tlf = tlf;
+    }
+
+    public override string ToString()
+    {
+        return $"{nameof(_navn)}: {_navn}, {nameof(_adresse)}: {_adresse}, {nameof(_tlf)}: {_tlf}";
+    }
 }
