a1462c23000db98a1cbe32f55ecdf17d16294f6d
Brinchiskii <sebastianbrinch.mail@gmail.com>
2023-10-31 18:12:01 +0100

    Drikkevarer repo

---
+++ NewMenuPizza/Services/DrikkevarerRepository.cs
@@ -0,0 +1,23 @@
+using NewMenuPizza.Drikkevarer;
+
+namespace NewMenuPizza.Services
+{
+    public class PizzaRepository
+    {
+        /*
+         * Instance field
+         */
+        
+        /*
+         * Properties
+         */
+        
+        /*
+         * Constructor
+         */
+        
+        /*
+         * Methods
+         */
+    }
+}
