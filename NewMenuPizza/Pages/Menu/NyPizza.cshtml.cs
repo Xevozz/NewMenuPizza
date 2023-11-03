b302085cf0a736255be668692eb5cfa9c3bda163
Abdirahman Abbas Sheikh Husein <abs001@edu.zealand.dk>
2023-11-03 10:31:44 +0100

    Opret en ny pizza.

---
+++ NewMenuPizza/Pages/Menu/NyPizza.cshtml.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using NewMenuPizza.PizzaFolderTest;
+using NewMenuPizza.Services;
+
+namespace NewMenuPizza.Pages.Menu
+{
+    public class NyPizzaModel : PageModel
+    { 
+        private PizzaRepository _pizzarepo;
+
+        public NyPizzaModel(PizzaRepository pizzarepo)
+        {
+            _pizzarepo = pizzarepo;
+        }
+
+        [BindProperty]
+        public string NyPizza { get; set; }
+
+        [BindProperty]
+        public int NyPris { get; set; }
+
+        [BindProperty]
+        public int NytNummer { get; set; }
+       
+        
+        public void OnGet()
+        {
+        }
+
+        public void OnPost() 
+        {
+            Pizza nyPizza = new Pizza(NyPizza, NyPris, NytNummer);
+           
+            PizzaRepository pizzaRepository = new PizzaRepository(true);
+            _pizzarepo.Tilføj(nyPizza);
+        }
+    }
+}
